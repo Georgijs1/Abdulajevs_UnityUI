47f11afce63a065ad364aa72d9ba3aaf92c5d7c6
Georgijs <lakawije@gmail.com>
2021-03-18 21:35:07 +0200

    Tika pievienota iespēja pārvietot objektus (apģērbus) ar peli

--- Assets/Skripti/ObjektuParvietosana.cs
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ObjektuParvietosana : MonoBehaviour
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private RectTransform transformacijuLogs;
+    public Canvas kanva;
+
+    private void Awake()
     {
-        
+        transformacijuLogs = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData notikums)
     {
-        
+        Debug.Log("Kreisais klikšķis uz pārvietojamā objekta!");
+    }
+
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Uzsākta pārvietošana!");
+    }
+
+    public void OnDrag(PointerEventData notikums)
+    {
+        transformacijuLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData notikums)
+    {
+        Debug.Log("Vilkšana pabeigta!");
     }
 }
