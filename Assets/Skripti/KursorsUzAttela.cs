a183f88ed1babd606f1e4824907cea80b8bf7722
Georgijs <lakawije@gmail.com>
2021-03-18 22:51:59 +0200

    Skaņu un Scroll View elementu pievienošana

---
+++ Assets/Skripti/KursorsUzAttela.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class KursorsUzAttela : MonoBehaviour
+{
+    public AudioSource skanasAvots;
+    public AudioClip skanaKoAtskanot;
+
+    public void uzbiditUzAttela(){
+        skanasAvots.PlayOneShot(skanaKoAtskanot);
+    }
+
+    public void kursorsNoietNoAttela(){
+        skanasAvots.Stop();
+    }
+}
